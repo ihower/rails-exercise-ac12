c8eaabef9b6ce270aa33eaa378fc3bdca3f5fd47
Wen-Tien Chang <ihower@gmail.com>
2016-12-08 10:43:02 +0800

    server-side render json

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,11 +1,11 @@
 class CatsController < ApplicationController
 
   def index
+    @cats = Cat.all
+
     respond_to do |format|
       format.html
       format.json {
-        @cats = Cat.all
-
         # Or use jbuilder
         render :json => @cats.map{ |x| { :id => x.id, :name => x.name, :image => x.image, :counter => x.counter } }.to_json
       }
