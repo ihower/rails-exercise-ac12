4bd7795575b8db3692aaebd65fcaedcc3fe87c8a
Wen-Tien Chang <ihower@gmail.com>
2016-12-08 10:22:15 +0800

    Add cat clicker

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/cats' => "cats#index"
 
   resources :products
 
   resources :orders
-  
+
   resource :cart do
     member do
       post :add_product
