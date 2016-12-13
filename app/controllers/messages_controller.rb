1820d31d9d431993fb83560583c285756de97e18
Wen-Tien Chang <ihower@gmail.com>
2016-12-13 11:37:24 +0800

    error handleing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,18 @@ class MessagesController < ApplicationController
   before_action :set_room
 
   def create
-    @message = @room.messages.create!( :content => params[:message][:content] )
+    @message = @room.messages.new( :content => params[:message][:content] )
 
-    # data = { :content => @message.content, :created_at => @message.created_at, :user_name => "Foobar" }
-    html = ApplicationController.renderer.render( :partial => "messages/message", :locals => { :message => @message } )
+    if @message.save
+      # data = { :content => @message.content, :created_at => @message.created_at, :user_name => "Foobar" }
+      html = ApplicationController.renderer.render( :partial => "messages/message", :locals => { :message => @message } )
 
-    ActionCable.server.broadcast( "room_#{@room.id}", html )
+      ActionCable.server.broadcast( "room_#{@room.id}", html )
 
-    render :js => '$("#message_content").val("");'
+      render :js => '$("#message_content").val("");'
+    else
+      render :js => 'alert("No content")';
+    end
   end
 
   protected
