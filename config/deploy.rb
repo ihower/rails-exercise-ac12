d45b78965d948d38b4e4cbde064fcf7ad4caf197
Wen-Tien Chang <ihower@gmail.com>
2016-12-23 12:16:57 +0800

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 `ssh-add`
 
 # config valid only for current version of Capistrano
-lock '3.6.1'
+lock '3.7.1'
 
 set :application, 'rails_exercise'
 set :repo_url, 'git@github.com:ihower/rails-exercise-ac12.git'
